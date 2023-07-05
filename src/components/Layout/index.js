7ae2f87da55f8bbb8e86eb1fc5260410cbcfee5c
rgabriel01 <raymond.m.gabriel@gmail.com>
2023-07-05 17:56:51 +0800

    adds initial cleanup and antd library

---
+++ src/components/Layout/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Col, Row } from 'antd'
+
+const Layout = () => {
+  return (
+    <Row>
+      <Col>
+        HENLOOO
+      </Col>
+    </Row>
+  )
+}
+
+export default Layout
